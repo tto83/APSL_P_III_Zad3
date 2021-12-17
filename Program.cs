40dae8ecb11bff19f3b77255cd19f98d5934e273
tto83 <t.j.tomasz@gmail.com>
2021-12-17 23:43:32 +0100

    init commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace APSL_P_III_Zad3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
